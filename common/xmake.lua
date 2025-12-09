4917946aa3818395609e511d970a8ad6febaeea6
Wang <mwang740@gatech.edu>
2025-12-09 15:55:25 -0500

    update compilation env for 12.9

--- common/xmake.lua
+++ common/xmake.lua
@@ -1,6 +1,6 @@
 set_languages("c++17")
 
-add_requires("fmt =8.1.1")
+--add_requires("fmt =8.1.1")
 add_requires("nlohmann_json >=3.10.5")
 --add_requires("boost =1.78.0", {configs = {cmake = false}})
 --add_requires("boost >=1.86.0")
--- xmake.lua
+++ xmake.lua
@@ -1,14 +1,21 @@
 add_rules("mode.debug", "mode.release", "mode.releasedbg")
 set_languages("c++17")
 
+add_requires("fmt =12.1.0")
+add_requireconfs("*.fmt", { override = true, version = "12.1.0" })
+
 includes("./common/xmake.lua")
 
 add_requires("eigen >=3.4.0")
 --add_requires("cuda")
-add_requires("vtk =9.3.1", {configs = {cuda = true}})
+add_requires("vtk >=9.5.1", {configs = {cuda = true}})
+--add_requires("vtk =9.3.1", {configs = {cuda = true}})
 --add_requires("stb")
---add_requires("polyscope =2.3")
+add_requires("polyscope =2.3")
 --add_requireconfs("polyscope.imgui", {override = true, version = "1.91.1"})
+
+add_defines("FMT_UNICODE=0")
+
 
 set_rundir("$(projectdir)")
 
